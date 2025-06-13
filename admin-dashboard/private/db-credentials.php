bb23b6e53259cd142a41357d494bbf842685d408
kyle-methoddata <kyle@methoddata.com>
2025-06-13 20:59:56 +0200

    Removed other file from tracking

--- admin-dashboard/private/db-credentials.php
+++
@@ -1,7 +0,0 @@
-<?php 
-  $hostName = "sql206.infinityfree.com";
-  $username = "if0_39223848";
-  $password = "DX9b34qZzA371C";
-  $adminDb = "if0_39223848_c2c_admin";
-  $c2cDb = "if0_39223848_c2c_db";
-?>
